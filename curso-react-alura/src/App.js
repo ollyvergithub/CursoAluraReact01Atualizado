5bb752b2374d50e7b6eca3a595edb5a133273528
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-21 17:34:33 -0300

    Aula_03_02 02

--- curso-react-alura/src/App.js
+++ curso-react-alura/src/App.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import Tabela from './Componentes/Tabela'
 
-function App() {
+class App extends React.Component{
 
-    const autores =  [
-        {
-            nome: 'Paulo',
-            livro: 'React',
-            preco: '1000'
-        },
-        {
-            nome: 'Daniel',
-            livro: 'Java',
-            preco: '99'
-        },
-        {
-            nome: 'Marcos',
-            livro: 'Design',
-            preco: '150'
-        },
-        {
-            nome: 'Bruno',
-            livro: 'DevOps',
-            preco: '100'
-        }
-    ];
+    state = {
+        autores:[
+            {
+                nome: 'Paulo',
+                livro: 'React',
+                preco: '1000'
+            },
+            {
+                nome: 'Daniel',
+                livro: 'Java',
+                preco: '99'
+            },
+            {
+                nome: 'Marcos',
+                livro: 'Design',
+                preco: '150'
+            },
+            {
+                nome: 'Bruno',
+                livro: 'DevOps',
+                preco: '100'
+            },
+            {
+                nome: 'Bruno',
+                livro: 'DevOps',
+                preco: '100'
+            }
+        ],
+    }
 
-  return (
-    <div>
-        <Tabela autores={autores}/>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                <Tabela autores={this.state.autores}/>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
